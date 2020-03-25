2e0258c0adc6b041ef0d19a8213718ce7d64e75d
Alexander Teslovskiy <artoriousso@gmail.com>
2020-03-25 18:28:57 +0300

    Fix params

--- src/parse/AST/values/graph.py
+++ src/parse/AST/values/graph.py
@@ -2,8 +2,8 @@ from parse.AST.values.value import Value
 
 
 class Graph(Value):
-    def __init__(self, nodes=[], arcs=[], graphs=[], cast=None):
-        self.nodes = nodes # []
-        self.arcs = arcs
-        self.graphs = graphs
+    def __init__(self, nodes=(), arcs=(), graphs=(), cast=None):
+        self.nodes = list(nodes)
+        self.arcs = list(arcs)
+        self.graphs = list(graphs)
         super().__init__(cast)
