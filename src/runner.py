9d77b4c5cc03fcfb75ca4a3aed042bad36611c9e
Alexander Teslovskiy <artoriousso@gmail.com>
2020-04-25 19:19:45 +0300

    Add basepath to runner script

--- src/runner.py
+++ src/runner.py
@@ -8,13 +8,16 @@ from compiler.compiler import Compiler
 BUILTINS_FILE = "src/runtime/builtins_signatures.sanya"
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         print("Error! Provide source and target files as argunents")
         exit(1)
 
     code_file = sys.argv[1]
     target_file = sys.argv[2]
+    directory = sys.argv[3]
 
-    ast = Parser(code_file, [BUILTINS_FILE]).parse()
+    builtins = directory + "/" + BUILTINS_FILE
+
+    ast = Parser(code_file, [builtins]).parse()
     if Analyzer().validate(ast):
         Compiler(target_file).compile(ast)
